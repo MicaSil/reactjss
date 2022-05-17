330a0c5adf1733c7f6ce59d769bc3a05d3e09d31
Micaela Silvera <micaelasilvera24@gmail.com>
2022-05-16 21:22:26 -0300

    crear NavBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './componentes/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Reactjs</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavBar/>
+        
       </header>
     </div>
   );
---
+++ src/componentes/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Infinite</h1>
+            <div>
+                <button>Damas</button>
+                <button>Hombres</button>
+                <button>Niños</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
