60a6ddfe9ead75b6d5271e047e77127d202e0e12
Micaela Silvera <micaelasilvera24@gmail.com>
2022-05-18 20:36:01 -0300

    Integrando el icono del carrito

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './componentes/NavBar/NavBar';
+import ItemListContainer from './componentes/ItemListContainer.js/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
-        
+        <ItemListContainer greeting="Bienvenidos a mi tienda" />
       </header>
     </div>
   );
--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -1,15 +1,17 @@
 import './NavBar.css'
+import CartWidget from '../CartWidget.js/CartWidget'
 
 const NavBar = () => {
     return (
-        <nav>
-            <h1>Infinite</h1>
-            <div>
-                <button>Damas</button>
-                <button>Hombres</button>
-                <button>Niños</button>
-            </div>
-        </nav>
+            <nav>
+                <h1>Infinite</h1>
+                <div>
+                    <button>Damas</button>
+                    <button>Hombres</button>
+                    <button>Niños</button>
+                </div>
+                <CartWidget/>
+            </nav>
     )
 }
 
